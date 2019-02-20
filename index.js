58317985dbc22913032fa31ecf33bb0190e03d7d
Rowan Taylor <rowan.taylor@colorado.edu>
2019-02-20 23:50:28 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+// function capitalizes
+
+function shout(string) {
+  return string.toUpperCase();
+};
+
+// function makes lowercase
+
+function whisper(string) {
+  return string.toLowerCase();
+};
+
+// function capitalizes and logs
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+};
+
+// function makes lowercase and logShout
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+};
